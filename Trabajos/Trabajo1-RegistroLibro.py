75d3e57e7a0e42b0b8d3943a8394c012a181b3d7
VANESA97 <113323129+VANESAHUALLPA@users.noreply.github.com>
2022-11-06 02:50:37 -0500

    Update Trabajo1-RegistroLibro.py
    Descripcion del trabajo

--- Trabajos/Trabajo1-RegistroLibro.py
+++ Trabajos/Trabajo1-RegistroLibro.py
@@ -1,5 +1,21 @@
 #Aquí se trabajara el sistema de registro de libros.
 
+#Se tiene la clase Libro con los siguientes atributos: id, título, género, ISBN, editorial y autor(es). Considerar que un libro puede tener varios autores.
+#Se solicita escribir un programa en Python que permita registrar libros. Debe utilizar: colecciones (listas, tuplas, etc), funciones y clases de Python.
+#Dicho programa debe tener un menu (a interactuar en la línea de comando) para:
+
+# Opción 1: Leer archivo de disco duro (.txt o csv) que cargue 3 libros.
+# Opción 2: Listar libros.
+# Opción 3: Agregar libro.
+# Opción 4: Eliminar libro.
+# Opción 5: Buscar libro por ISBN o por título. Se debe sugerir las opciones y listar el resultado.
+# Opción 6: Ordenar libros por título.
+# Opción 7: Buscar libros por autor, editorial o género. Se deben sugerir las opciones y listar los resultados.
+# # Opción 8: Buscar libros por número de autores. Se debe ingresar un número por ejemplo 2 (hace referencia a dos autores) y se deben listar todos los libros que contengan 2 autores.
+# # Opción 9: Editar o actualizar datos de un libro (título, género, ISBN, editorial y autores).
+# Opción 10: Guardar libros en archivo de disco duro (.txt o csv).
+# Nota: listar libros involucra: título, género, ISBN, editorial y autor(es)
+
 
 class libro:
     
