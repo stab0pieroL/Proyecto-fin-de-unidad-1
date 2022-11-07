0f9fdf69f12866e19c7aa93e6069ef43fbfdb504
stab0pieroL <rake.pumpkinhead.96@gmail.com>
2022-11-06 22:45:51 -0500

    Primera parte revisionada de la pokeapi

--- Trabajos/Trabajo2-Pokeapi.py
+++ Trabajos/Trabajo2-Pokeapi.py
@@ -1,27 +1,42 @@
 import requests
 import json
 
-print("\t\t.:GENERACION POKEMON:.")
-print("1.Generation-1\n"
-      "2.Generation-2\n"
-      "3.Generation-3\n"
-      "4.Generation-4\n"
-      "5.Generation-5\n"
-      "6.Generation-6\n"
-      "7.Generation-7\n"
-      "8.Generation-8\n")
 
-link_pokiapi1 = 'https://pokeapi.co/api/v2/generation/'
 
-user_input1=input("Introduce la generacion: ")
+def menu():
 
-def lista_pokemon(id1):
-    resp = requests.get(link_pokiapi1+id1)
+    print("\n- - - - - - - - Bienvenido al programa de listado de Pokemon - - - - - - -")
+    print("Elige la opcion segun el metodo de listado preferido.")
+    print("[1] Opcion por generación")
+    print("[2] Opcion por forma")
+    print("[3] Opcion por habilidad")
+    print("[4] Opcion por habitad")
+    print("[5] Opcion por tipo")
+    print("[6] Salir")
 
-    data = resp.json()
+def option_1():
 
-    gen_pokemon = [gen['name'] for gen in data['pokemon_species']]
-    print(f"Pokemon: {gen_pokemon} ")
+        print("\t\t.:GENERACION POKEMON:.")
+        print("1.Generation-1\n"
+        "2.Generation-2\n"
+        "3.Generation-3\n"
+        "4.Generation-4\n"
+        "5.Generation-5\n"
+        "6.Generation-6\n"
+        "7.Generation-7\n"
+        "8.Generation-8\n")
 
-for id1 in user_input1:
-    lista_pokemon(id1) 
+        link_pokiapi1 = 'https://pokeapi.co/api/v2/generation/'
+
+        user_input1=input("Introduce la generacion: ")
+
+        def lista_pokemon(id1):
+            resp = requests.get(link_pokiapi1+id1)
+
+            data = resp.json()
+
+            gen_pokemon = [gen['name'] for gen in data['pokemon_species']]
+            print(f"Pokemon: {gen_pokemon} ")
+
+        for id1 in user_input1:
+            lista_pokemon(id1)
