bd31a6e2a5a9af672c24a8432fa87f10471db163
VANESA97 <vhuallpasayritupa@gmail.com>
2022-11-06 22:56:20 -0500

    Avance final de mejora

--- Trabajos/Trabajo2-Pokeapi.py
+++ Trabajos/Trabajo2-Pokeapi.py
@@ -1,42 +1,63 @@
 import requests
 import json
 
-print("\t\t.:GENERACION POKEMON:.")
-print("1.Generation-1\n"
-      "2.Generation-2\n"
-      "3.Generation-3\n"
-      "4.Generation-4\n"
-      "5.Generation-5\n"
-      "6.Generation-6\n"
-      "7.Generation-7\n"
-      "8.Generation-8\n")
 
-link_pokiapi1 = 'https://pokeapi.co/api/v2/generation/'
 
-user_input1=input("Introduce la generacion: ")
+def menu():
 
-def lista_pokemon(id1):
-    resp = requests.get(link_pokiapi1+id1)
+    print("\n- - - - - - - - Bienvenido al programa de listado de Pokemon - - - - - - -")
+    print("Elige la opcion segun el metodo de listado preferido.")
+    print("[1] Opcion por generación")
+    print("[2] Opcion por forma")
+    print("[3] Opcion por habilidad")
+    print("[4] Opcion por habitad")
+    print("[5] Opcion por tipo")
+    print("[6] Salir")
 
-    data = resp.json()
+def option_1():
 
-    gen_pokemon = [gen['name'] for gen in data['pokemon_species']]
-    print(f"Pokemon: {gen_pokemon} ")
+        print("\t\t.:GENERACION POKEMON:.")
+        print("1.Generation-1\n"
+        "2.Generation-2\n"
+        "3.Generation-3\n"
+        "4.Generation-4\n"
+        "5.Generation-5\n"
+        "6.Generation-6\n"
+        "7.Generation-7\n"
+        "8.Generation-8\n")
 
-for id1 in user_input1:
-    lista_pokemon(id1) 
+        link_pokiapi1 = 'https://pokeapi.co/api/v2/generation/'
 
-user_input2=input("Ingrese la forma que desea : ")
+        user_input1=input("Introduce la generacion: ")
 
-link_pokiapi2='https://pokeapi.co/api/v2/pokemon-shape/'
+        def lista_pokemon(id1):
+            resp = requests.get(link_pokiapi1+id1)
 
-def forma_pokemon(id2):
-    resp = requests.get(link_pokiapi2+id2)
+            data = resp.json()
 
-    data = resp.json()
+            gen_pokemon = [gen['name'] for gen in data['pokemon_species']]
+            print(f"Pokemon: {gen_pokemon} ")
 
-    forma_pokemon=[nombre['name'] for nombre in data['pokemon_species']]
-    print(f"Lists de pokemon por forma: {forma_pokemon}")
+        for id1 in user_input1:
+            lista_pokemon(id1)
+            
+def option_2():
 
-for id2 in user_input2:
-    forma_pokemon(id2)
+        print("\t\t.:FORMA POKEMON:.")
+        print("Elija un numero del 1 al 14 para seleccionar el listado por forma que desea.")
+
+
+        user_input2=input("Ingrese la forma que desea : ")
+
+        link_pokiapi2='https://pokeapi.co/api/v2/pokemon-shape/'
+
+        def forma_pokemon(id2):
+            resp = requests.get(link_pokiapi2+id2)
+
+            data = resp.json()
+
+            forma_pokemon=[forma['name'] for forma in data['pokemon_species']]
+            print(f"Lists de pokemon por forma: {forma_pokemon}")
+
+        for id2 in user_input2:
+            forma_pokemon(id2)
